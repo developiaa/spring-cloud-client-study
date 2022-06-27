863c5e176aebdd8e0d70df1b835f291ef4135f8a
developiaa <developiaa@gmail.com>
2022-06-27 23:43:15 +0900

    first commit

---
+++ src/test/java/pro/developia/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.developia.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
