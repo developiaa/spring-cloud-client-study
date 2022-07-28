c6fa3c26b5bcb69d989b022f0c61ef22151abbe6
developiaa <developiaa@gmail.com>
2022-07-28 23:55:48 +0900

    feat : Add gateway uri

--- src/main/java/pro/developia/userservice/controller/UserController.java
+++ src/main/java/pro/developia/userservice/controller/UserController.java
@@ -22,12 +22,12 @@ public class UserController {
     private final Greeting greeting;
     private final UserService userService;
 
-    @GetMapping("/health-check")
+    @GetMapping("/user-service/health-check")
     public String status() {
-        return "It's Working in User service";
+        return String.format("It's Working in User service %s", env.getProperty("local.server.port"));
     }
 
-    @GetMapping("/welcome")
+    @GetMapping("/user-service/welcome")
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
