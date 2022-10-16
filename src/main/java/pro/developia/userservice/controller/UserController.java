79398400e01606eb42aecaf7984991d0212ee9ea
developiaa <developiaa@gmail.com>
2022-10-16 20:25:15 +0900

    feat: Add micrometer config

--- src/main/java/pro/developia/userservice/controller/UserController.java
+++ src/main/java/pro/developia/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package pro.developia.userservice.controller;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -27,6 +28,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/health-check")
+    @Timed(value = "users.status", longTask = true)
     public String status() {
         return String.format("It's Working in User service "
                 + ", port(local.server.port)=" + env.getProperty("local.server.port")
@@ -37,6 +39,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "users.welcome", longTask = true)
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
